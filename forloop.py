c7fe3413e8c966034cc868f4ca49ef09c20f92b0
Shubham Mishra <subham.mishra2433@gmail.com>
2019-07-07 11:59:28 +0530

    tryal of for loop with dict

---
+++ forloop.py
@@ -0,0 +1,9 @@
+l=[2,'ruirn',6.7,4+7j,'lol']
+di={'r2f':'4545','upo': l}
+vt=0
+for v in dict(di):
+    print("chalo try karte hi=======*********",v[vt])
+    vt+=1
+else:
+    print("opsssssss!!!!!!!1 sorry")
+
