78b52bca00db0c746d2ab1c2d1dc11e42a2fc627
Shubham Mishra <subham.mishra2433@gmail.com>
2019-07-08 10:18:09 +0530

    simple operator concept

---
+++ operator concept.py
@@ -0,0 +1,27 @@
+A=10.2
+b=110
+c=2
+s="klarpatuu"
+d="keral"
+aa="kalakande"
+print("\n\n\n<---------------------------------THIS IS AIRTHMATIC------------------------------->\n\n\n\n")
+print("A+b==",A+b)
+print("s+d+aa",s[0]+d+aa[0:3])
+print("//",b//A)
+print("/",b/A)
+print("\n\n\n<---------------------------------THIS IS RELATION------------------------------->\n\n\n\n")
+if(True == False):
+    print("True != False")
+    if(10<50<55<61.7>61.8):
+        print("10<50<55<61.7>61.8")
+    else:print("!!!!!!!!!!!!!10<50<55<61.7>61.8")
+else:
+    print("True == False")
+    if('a'=='A'):
+        print("'a'=='A'")
+    else:print("'a'!='A'")
+print("\n\n\n<---------------------------------THIS IS LOGICAL------------------------------->\n\n\n\n")
+print("'A' and 90==",'A' and 90)
+print('10 AND 20 AND 0===',10 and 20 and 0)
+print('\n\n110 or 20 or 10===',10 or 20 or 0)
+print('\n\n10 or 20 or "AS"===',10 or 20 or 0)
