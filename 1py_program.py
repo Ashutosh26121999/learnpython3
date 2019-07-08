453a8f39b0acbf6b2aed5f8e8b5e113c9f6e81c2
Shubham Mishra <subham.mishra2433@gmail.com>
2019-07-08 10:26:28 +0530

    1 simple python 3 program

---
+++ 1py_program.py
@@ -0,0 +1,22 @@
+print("hello python 3.o")
+a=12
+b=3.5
+c=True
+d='ram'
+
+print(a,"==", type(a), b,"==",type (b),c,"==",type(c),d,"==",type(d), "end")
+swep=a
+a=b
+b=c
+c=d
+d=swep
+print("After sweap")
+print(a , type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+
+ABC=20
+print(ABC)
+ABC=100
+print(ABC)
